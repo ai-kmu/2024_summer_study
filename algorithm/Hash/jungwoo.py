d648cf1cbcd53563bced13be0a774b5d48b2891d
YeormDal <summermuun@gmail.com>
2024-07-18 20:58:36 +0900

    Create jungwoo.py

---
+++ algorithm/Hash/jungwoo.py
@@ -0,0 +1,13 @@
+def lengthOfLongestSubstring(s: str) -> int:
+    char_set = set()
+    left = 0
+    max_length = 0
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+        char_set.add(s[right])
+        max_length = max(max_length, right - left + 1)
+
+    return max_length
