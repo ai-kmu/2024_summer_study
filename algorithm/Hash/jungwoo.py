9de03b4bcc7f4b19f4fa12428ec4bd116194cf2e
YeormDal <summermuun@gmail.com>
2024-07-18 21:01:04 +0900

    Update jungwoo.py

--- algorithm/Hash/jungwoo.py
+++ algorithm/Hash/jungwoo.py
@@ -1,13 +1,14 @@
-def lengthOfLongestSubstring(s: str) -> int:
-    char_set = set()
-    left = 0
-    max_length = 0
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        char_set = set()
+        left = 0
+        max_length = 0
 
-    for right in range(len(s)):
-        while s[right] in char_set:
-            char_set.remove(s[left])
-            left += 1
-        char_set.add(s[right])
-        max_length = max(max_length, right - left + 1)
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+            char_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
 
-    return max_length
+        return max_length
