3ad75ce603bf25dbfc3d9fc0e5d03bcdec74cef2
kwonbooyeon <qndusrnjs@gmail.com>
2024-07-30 19:04:19 +0900

    [update_booyeon.py] review 추가 - (algorithm/greedy/booyeon.py)

--- algorithm/greedy/booyeon.py
+++ algorithm/greedy/booyeon.py
@@ -10,3 +10,14 @@ class Solution:
             result += 1
 
         return result
+        
+        
+'''
+다른 분들은 
+line 7의 coins = coins - cost 를 coins -= cost 로 줄이거나,
+if문을 for문 전체로 감싸는 형태,
+while문을 사용하거나 
+break대신 return문으로 한 형태가 있었다.
+
+greedy를 직관적이게 잘 설명한 문제지만, 이번엔 다들 풀이가 비슷할 수 밖에 없는 점이 아쉽다.
+'''
