749dd998558f246cc65881bcb93c32e1941dc4c2
YunHui Kim <106079352+kyunhui@users.noreply.github.com>
2024-08-08 15:51:05 +0900

    Create yunhui.py

---
+++ algorithm/Binary Search/yunhui.py
@@ -0,0 +1,23 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums)-1
+        
+        while(start <= end): 
+            mid =  (start + end) // 2
+            
+            if nums[mid] == target:
+                return mid
+            
+            if nums[start] <= nums[mid]:
+                if target >= nums[start] and target <= nums[mid]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+                    
+            else:
+                if nums[mid] <= target and nums[end] >= target:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+                
+        return -1
