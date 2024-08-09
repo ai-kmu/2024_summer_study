956cda1b0524ef7130094b37e61cd38dd9c468bd
Josungjun <66383669+Josungjun@users.noreply.github.com>
2024-08-09 14:27:05 +0900

    Update Sungjun.py

--- algorithm/DFSBFS/Sungjun.py
+++ algorithm/DFSBFS/Sungjun.py
@@ -14,21 +14,18 @@ class Solution:
             return ans
 
         def dfs(node, current_node, sum):
-
             #leaf node 이어야 함.
             if(node.left == None and node.right == None):
-
                 current_node += [node.val]
                 sum += node.val
-
+                #합격 조건
                 if(sum == targetSum):
                     ans.append(current_node)
                     return
-            
-            
+            #좌측 노드 탐색        
             if(node.left != None):
                 dfs(node.left, current_node + [node.val], sum + node.val)
-            
+            #우측 노드 탐색
             if(node.right != None):
                 dfs(node.right, current_node + [node.val], sum + node.val)
 
